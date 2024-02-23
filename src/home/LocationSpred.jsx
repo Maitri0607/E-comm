074a4ea3a0475c9562f903df47d9d1891fa60d23
Maitri0607 <milipatel555@gmail.com>
2024-02-24 00:09:32 +0530

    Changes

---
+++ src/home/LocationSpred.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const title = "More Then 60,000 Customers";
+
+const desc = "Buy products on your any device with our app & enjoy your time what you want. Just download & install & start to shopping";
+
+const clientsList = [
+    {
+        imgUrl: '/src/assets/images/clients/avater.jpg',
+        imgAlt: 'education thumb rajibraj91 rajibraj',
+        text: 'Join with Us',
+    },
+    {
+        imgUrl: '/src/assets/images/clients/avater.jpg',
+        imgAlt: 'education thumb rajibraj91 rajibraj',
+        text: 'Join with Us',
+    },
+    {
+        imgUrl: '/src/assets/images/clients/avater.jpg',
+        imgAlt: 'education thumb rajibraj91 rajibraj',
+        text: 'Join with Us',
+    },
+    {
+        imgUrl: '/src/assets/images/clients/avater.jpg',
+        imgAlt: 'education thumb rajibraj91 rajibraj',
+        text: 'Join with Us',
+    },
+    {
+        imgUrl: '/src/assets/images/clients/avater.jpg',
+        imgAlt: 'education thumb rajibraj91 rajibraj',
+        text: 'Join with Us',
+    },
+    {
+        imgUrl: '/src/assets/images/clients/avater.jpg',
+        imgAlt: 'education thumb rajibraj91 rajibraj',
+        text: 'Join with Us',
+    },
+    {
+        imgUrl: '/src/assets/images/clients/avater.jpg',
+        imgAlt: 'education thumb rajibraj91 rajibraj',
+        text: 'Join with Us',
+    },
+]
+
+
+const LocationSpred = () => {
+    return (
+        <div>
+            LocationSpred
+        </div>
+    )
+}
+
+export default LocationSpred
